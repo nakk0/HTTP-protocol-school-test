2c91fd1ed211ea2b96f2844d488f10a5767836e9
lai2-24 <lai2-24@itismeucci.com>
2024-01-10 13:49:23 +0100

    added image and docs, gotta fix 301

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -26,11 +26,22 @@ public class App {
                     System.out.println(message);
                 }
 
-                File file = new File(path);
+                if (path.equals("") || path.charAt(path.length() - 1) == '/') {
+                    path += "index.html";
+                }
+
+                File file = new File("docs/" + path);
+                System.out.println(path);
+
+                if(path.equals("test")){
+                    out.writeBytes("HTTP/1.1 301 MOVED PERMANENTLY\n");
+                    out.writeBytes("Location: https://google.com\n");
+                    out.writeBytes("\n");
+                }
 
                 if (file.exists()) {
                     System.out.println("the file exists");
-                    sendBinaryFile(file, out);
+                    sendBinaryFile(client, file);
                 } else {
                     message = "file not found";
                     System.out.println(message);
@@ -51,29 +62,39 @@ public class App {
         }
     }
 
-    public static void sendBinaryFile(File file, DataOutputStream out) {
-        try {
-            out.writeBytes("HTTP/1.1 200 OK\n");
-            out.writeBytes("Content-length: " + file.length() + "\n");
-            out.writeBytes("Content-type: " + getContentType(file) + "\n");
-            out.writeBytes("\n");
+    private static void sendBinaryFile(Socket socket, File file) throws IOException {
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-            String binary = Files.readString(file.toPath());
-            out.writeBytes(binary + "\n");
+        out.writeBytes("HTTP/1.1 200 OK\n");
+        out.writeBytes("Content-Length: " + file.length() + "\n");
+        out.writeBytes(getContentType(file) + "\n");
+        out.writeBytes("\n");
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        InputStream input = new FileInputStream(file);
+        byte[] buf = new byte[8192];
+        int n;
+        while ((n = input.read(buf)) != -1) {
+            out.write(buf, 0, n);
         }
-
+        input.close();
     }
 
-    public static String getContentType(File file) {
-        String mime = "application/octet-stream";
-        try {
-            mime = Files.probeContentType(file.toPath());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+    private static String getContentType(File file) {
+        String filename = file.getName();
+        String[] temp = filename.split("\\.");
+        String extension = temp[temp.length - 1];
+        switch (extension) {
+            case "html":
+                return "Content-Type: text/html";
+            case "png":
+                return "Content-Type: image/png";
+            case "jpeg":
+            case "jpg":
+                return "Content-Type: image/jpeg";
+            case "css":
+                return "Content-Type: text/css";
+            default:
+                return "Content-Type: text/plain";
         }
-        return mime;
     }
 }
